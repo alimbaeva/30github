9baf082360ee06c87e24d4339a89c9e26cd3fc63
Asel <asel.alimbaeva.kk@gmail.com>
2024-02-17 15:47:42 +0300

    feat add

---
+++ hangman_24/src/data/questions.js
@@ -0,0 +1,61 @@
+const questions = [
+  {
+    questions: 'What is the liquid used for drawing or writing?',
+    answer: 'paint',
+  },
+  {
+    questions:
+      'What is the name of the object used for writing that contains a rod?',
+    answer: 'pen',
+  },
+  {
+    questions:
+      'What does "insert a hook" mean when used to highlight important information?',
+    answer: 'underline',
+  },
+  {
+    questions: 'What is the name of the item used to sharpen pencils?',
+    answer: 'sharpener',
+  },
+  {
+    questions:
+      'What is a piece of paper with a written message that is transmitted?',
+    answer: 'letter',
+  },
+  {
+    questions:
+      'What is the name of the tiny object commonly used to hold sheets of paper?',
+    answer: 'paperclip',
+  },
+  {
+    questions:
+      'What does the combination of letters on a key mean to create a space between words?',
+    answer: 'space',
+  },
+  {
+    questions:
+      'What is the name of a drawing instrument consisting of a wooden body and a graphite rod?',
+    answer: 'pencil',
+  },
+  {
+    questions:
+      'What is the name of the wooden or plastic tool used to remove old writing from paper?',
+    answer: 'eraser',
+  },
+  {
+    questions:
+      'What is the name of a large piece of paper usually used for drawing or writing large texts?',
+    answer: 'arcana',
+  },
+  {
+    questions: 'What is a set of paints often used to create artwork?',
+    answer: 'palette',
+  },
+  {
+    questions:
+      'What is the name of the clear adhesive tape used to glue paper together?',
+    answer: 'scotch',
+  },
+];
+
+export default questions;
