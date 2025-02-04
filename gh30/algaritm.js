e9997e91aa4dd250fd7b93b7de4e2848ef3ce256
Asel <88249718+alimbaeva@users.noreply.github.com>
2025-02-04 09:34:20 +0300

    Gh 30 (#47)
    * feat: des obj
    
    * feat: add class

--- gh30/algaritm.js
+++ gh30/algaritm.js
@@ -9,4 +9,17 @@ const {cash: {name}} = obj
 const {name: nameis } = obj.cash
 
 console.log(name)
+
 console.log(nameis)
+
+
+class Animals {
+    component(types) {
+        this.type = types
+    }
+
+    get () {
+        console.log(this.type)
+    }
+}
+
