17ec035a7bee272350c067009e307b470d626be6
Asel <asel.alimbaeva.kk@gmail.com>
2025-02-04 09:32:41 +0300

    feat: add class

--- gh30/algaritm.js
+++ gh30/algaritm.js
@@ -10,3 +10,14 @@ const {name: nameis } = obj.cash
 
 console.log(name)
 console.log(nameis)
+
+
+class Animals {
+    component(types) {
+        this.type = types
+    }
+
+    get () {
+        console.log(this.type)
+    }
+}
